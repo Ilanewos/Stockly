b15048ba2afd2b9b63da0ccf742bb77cd5e15787
Ariningsih <162403769+Ariningsih@users.noreply.github.com>
2025-10-25 23:42:42 +0800

    menambahkan file controllers dan routes

--- backend/operasional/routes/orderRoutes.js
+++ backend/operasional/routes/orderRoutes.js
@@ -2,9 +2,17 @@
 const express = require('express');
 const router = express.Router();
 const {
-    listOrdersb
+    listOrders,
+    historyOrders,
+    getOrder,
+    summary,
+    updateStatus
 } = require('../controllers/orderControllers');
 
 router.get('/orders', listOrders);            // ?status=proses
+router.get('/orders/history', historyOrders);
+router.get('/orders/:id', getOrder);
+router.get('/orders-summary', summary);
+router.post('/orders/:id/status', updateStatus);
 
 module.exports = router;
