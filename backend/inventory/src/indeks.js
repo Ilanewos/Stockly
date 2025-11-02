0917d4506630a110533797a303fa9528e609aab6
Ilanewos <ilanewos04@gmail.com>
2025-11-02 13:58:12 +0800

    update index.js

--- backend/inventory/src/indeks.js
+++ backend/inventory/src/indeks.js
@@ -1,21 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const bahanRoutes = require('./routes/bahanRoutes');
-const restockRoutes = require('./routes/restockRoutes');
+const restokRoutes = require('./routes/restokRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-// index.js (backend)
-const cors = require('cors');
-app.use(cors());
-
-
+// Routes
 app.use('/api/bahan', bahanRoutes);
-app.use('/api/restock', restockRoutes);
+app.use('/api/restok', restokRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server berjalan di port ${PORT}`));
