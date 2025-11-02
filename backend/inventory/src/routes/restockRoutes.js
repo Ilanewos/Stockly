71dce3138f0edf1871fa16ed2e7217cd92937249
Ilanewos <ilanewos04@gmail.com>
2025-11-02 13:56:12 +0800

    updated restokRoute

--- backend/inventory/src/routes/restockRoutes.js
+++ backend/inventory/src/routes/restockRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const restockController = require('../controllers/restockController');
+const restokController = require('../controllers/restokController');
 
-router.get('/', restockController.getAll);
-router.get('/:id_restock', restockController.getById);
-router.post('/', restockController.createOrUpdate);
-router.put('/:id_restock', restockController.update);
-router.delete('/:id_restock', restockController.delete);
+router.get('/', restokController.getAllRestok);
+router.post('/', restokController.createRestok);
 
 module.exports = router;
